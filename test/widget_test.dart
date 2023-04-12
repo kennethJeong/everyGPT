b0678d1652e4c673ae04b94cc620cc358c1788cd
Eunhu Jeong <kennethj@naver.com>
2023-04-12 22:45:45 +0900

    Build initial settings.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gpts/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const GPT());
+    await tester.pumpWidget(GPT());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
